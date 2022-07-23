db1db7125002de4e25c1d11506f96dbf12d214fb
Erin <79354991+oati@users.noreply.github.com>
2022-07-23 06:48:23 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixCargoIntegration = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
+    root = ./.;
+  };
+}
