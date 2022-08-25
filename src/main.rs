4de0ddf219d132ca7c7f7c3f54a1d9d405c51bdb
Erin <79354991+oati@users.noreply.github.com>
2022-08-24 20:12:47 -0500

    update and improve usage message

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use std::env;
 use sway_workspace_manager::{run, Command, Workspaces};
 use swayipc::Connection;
 
+const USAGE: &str =  "\
+Usage:
+    sway-workspace-manager switch|move|create|move-to-new|swap [prev|next|start|end|<number>] [--cycle]
+    sway-workspace-manager reorder
+    sway-workspace-manager rename <new-name>
+";
+
 fn main() -> Result<(), swayipc::Error> {
     let mut connection = Connection::new()?;
 
     let command = match Command::new(env::args()) {
         Ok(command) => command,
         Err(msg) => {
-            eprintln!("Usage: sway-workspace-manager reorder|switch|move|create|move-to-new|swap [prev|next|start|end|<number>] [--cycle]\n");
+            eprintln!("{USAGE}");
             return Err(swayipc::Error::CommandFailed(format!("Parse error: {msg}")));
         }
     };
